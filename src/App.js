ef28b36dd5c58b81b1d1cc9f17ff8baa9834fc2d
fmwong77 <fongmeiw@gmail.com>
2020-05-17 10:04:48 -0500

    implement react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { HomePage } from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Switch>
+				<Route path="/" component={HomePage} />
+			</Switch>
+			{/* <HomePage /> */}
 		</div>
 	);
 }
