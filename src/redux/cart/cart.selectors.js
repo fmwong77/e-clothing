2ab9956a5e9e2bbf96a9f7943605c2f7d9864ff4
fmwong77 <fongmeiw@gmail.com>
2020-06-05 10:33:11 -0500

    added checkout page, checkout item component and updated cart to use selector from reselect

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,9 @@ export const selectCartItemsCount = createSelector(
 	(cartItems) =>
 		cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
 );
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+	cartItems.reduce(
+		(total, cartItem) => total + cartItem.quantity * cartItem.price,
+		0
+	)
+);
