4639f1d4f01ea10d61130c78b2b19785c018be4f
Fong Mei Wong <55044050+fmwong77@users.noreply.github.com>
2020-06-11 12:15:15 -0500

    Update collection-item.component.jsx
    Added $ to the price

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -14,7 +14,7 @@ const CollectionItem = ({ item, addItem }) => {
 			></div>
 			<div className="collection-footer">
 				<span className="title">{name}</span>
-				<span className="price">{price}</span>
+				<span className="price">${price}</span>
 			</div>
 			<CustomButton inverted onClick={() => addItem(item)}>
 				{' '}
